f6a5fa6cdba9faeaa3bdfdfc72de928abc5657d2
Rakib Uddin <16103030@iubat.edu>
2019-02-01 19:27:48 +0600

    Added All Dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from "react-router-dom"
+import Navbar from "./Components/Layout/Navbar"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+        </div>
+      </BrowserRouter>
     );
   }
 }
